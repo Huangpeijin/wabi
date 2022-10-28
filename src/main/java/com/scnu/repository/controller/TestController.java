089dc8a073142c03f25123b1c9bab78b3660c1ff
Huangpeijin <kim475@163.com>
2022-10-28 22:33:40 +0800

    2-5 增加logback配置文件

---
+++ src/main/java/com/scnu/repository/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.scnu.repository.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "hello world";
+    }
+}
