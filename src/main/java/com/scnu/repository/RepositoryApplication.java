bf77dcb817142f3c4c005c1a70443ebdcafe6a8d
Huangpeijin <kim475@163.com>
2022-10-28 15:16:26 +0800

    2init

---
+++ src/main/java/com/scnu/repository/RepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.scnu.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scnu/repository/RepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scnu.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
