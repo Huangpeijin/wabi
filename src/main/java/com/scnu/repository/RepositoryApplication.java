d5a5efd07a89f549982ca54800f2e0c7f0210f88
Huangpeijin <kim475@163.com>
2022-10-30 21:58:00 +0800

    2-5 修改启动文案

--- src/main/java/com/scnu/repository/RepositoryApplication.java
+++ src/main/java/com/scnu/repository/RepositoryApplication.java
@@ -1,13 +1,18 @@
 package com.scnu.repository;
 
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class RepositoryApplication {
-
+	private static final Logger LOG = (Logger) LoggerFactory.getLogger(RepositoryApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(RepositoryApplication.class, args);
+		SpringApplication app = new SpringApplication(RepositoryApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
 	}
-
 }
