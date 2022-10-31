2b831200c6c08a557f4791e9b829df56111751f7
Huangpeijin <kim475@163.com>
2022-10-31 08:25:06 +0800

    2-6开发Hello World接口 访问地址：http://127.0.0.1:8880/hello

--- src/main/java/com/scnu/repository/config/RepositoryApplication.java
+++ src/main/java/com/scnu/repository/config/RepositoryApplication.java
@@ -1,11 +1,14 @@
-package com.scnu.repository;
+package com.scnu.repository.config;
 
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@ComponentScan("com.scnu")
 @SpringBootApplication
 public class RepositoryApplication {
 	private static final Logger LOG = (Logger) LoggerFactory.getLogger(RepositoryApplication.class);
--- src/main/java/com/scnu/repository/controller/TestController.java
+++ src/main/java/com/scnu/repository/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.scnu.repository.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String hello(){
-        return  "hello world";
+        return  "hello world!";
     }
 }
