a9bd24949fb84a95af161c27a7c45e8e5e01eb9a
Huangpeijin <kim475@163.com>
2022-11-01 16:05:56 +0800

    2-7增加hello接口测试test.http

--- src/main/java/com/scnu/repository/controller/TestController.java
+++ src/main/java/com/scnu/repository/controller/TestController.java
@@ -1,9 +1,7 @@
 package com.scnu.repository.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +10,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return  "hello world!";
+    }
+    @PostMapping ("/hello/post")
+    public String helloPost(String name){
+        return  "hello world! Post,"+name;
     }
 }
