0c3fec454dcd2e80b3249293c0bb476391141100
Huangpeijin <kim475@163.com>
2022-11-01 16:38:42 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/scnu/repository/controller/TestController.java
+++ src/main/java/com/scnu/repository/controller/TestController.java
@@ -1,15 +1,18 @@
 package com.scnu.repository.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private  String testhello;
 
     @GetMapping("/hello")
     public String hello(){
-        return  "hello world!";
+        return  "hello world!"+testhello;
     }
     @PostMapping ("/hello/post")
     public String helloPost(String name){
