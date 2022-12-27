645709e47011c168fd7c776ab8fa6e11c44b66aa
Huangpeijin <kim475@163.com>
2022-12-27 21:24:44 +0800

    5-2集成axios，使用axios调用后端接口/ebook/list

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,91 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <a-layout>
+      <a-layout-header class="header">
+        <div class="logo" />
+        <a-menu
+          theme="dark"
+          mode="horizontal"
+          v-model:selectedKeys="selectedKeys1"
+          :style="{ lineHeight: '64px' }"
+        >
+          <a-menu-item key="1">电子书管理</a-menu-item>
+          <a-menu-item key="2">文档管理</a-menu-item>
+          <a-menu-item key="3">分类管理</a-menu-item>
+        </a-menu>
+      </a-layout-header>
+        <a-layout-sider class="sider" width="200" style="background: #fff">
+          <a-menu
+            mode="inline"
+            v-model:selectedKeys="selectedKeys2"
+            v-model:openKeys="openKeys"
+            :style="{ height: '100%', borderRight: 0 }"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  <user-outlined />
+                  计算机
+                </span>
+              </template>
+              <a-menu-item key="1">Java</a-menu-item>
+              <a-menu-item key="2">SprinBoot</a-menu-item>
+              <a-menu-item key="3">Vue</a-menu-item>
+              <a-menu-item key="4">VueCLI</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  <laptop-outlined />
+                  高等数学
+                </span>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
+                <span>
+                  <notification-outlined />
+                  脑科学
+                </span>
+              </template>
+              <a-menu-item key="9">option9</a-menu-item>
+              <a-menu-item key="10">option10</a-menu-item>
+              <a-menu-item key="11">option11</a-menu-item>
+              <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            Content
+        </a-layout-content>
+      <a-layout-footer style="text-align: center">
+       Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+ .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 24px;
+  background: rgba(255, 255, 255, 0.3);
+}
+.sider{
+  float: left;
+}
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.site-layout-background {
+  background: #fff;
 }
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -11,11 +11,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import axios from 'axios';
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+  setup(){
+        console.log("setup");
+        axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
+            console.log(response);
+        })
+    }
+   });
 </script>
