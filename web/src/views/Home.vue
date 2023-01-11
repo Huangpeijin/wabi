2c49cfbb5234e8e0d605acddff450a4fe4d14cc3
Huangpeijin <kim475@163.com>
2023-01-11 19:06:29 +0800

    5-5 axios请求域改为读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -94,8 +94,8 @@ export default defineComponent({
       const ebooks =ref();
       const ebooks1 =reactive({books:[]});
       onMounted(()=>{
-        console.log("onMounted");
-        axios.get("http://localhost:8880/ebook/list").then((response)=>{
+        console.log("onMounted1111");
+        axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response)=>{
           const data=response.data;
           ebooks.value=data.content;
           ebooks1.books=data.content;
