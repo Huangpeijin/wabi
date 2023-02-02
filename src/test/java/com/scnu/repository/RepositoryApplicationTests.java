e9d9742b4755757a52708f74a7280049827f608b
Huangpeijin <kim475@163.com>
2023-02-02 17:47:06 +0800

    5-8 增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器、拦截器

--- src/test/java/com/scnu/repository/RepositoryApplicationTests.java
+++ src/test/java/com/scnu/repository/RepositoryApplicationTests.java
@@ -1,8 +1,9 @@
 package com.scnu.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@SpringBootConfiguration
 @SpringBootTest
 class RepositoryApplicationTests {
 
