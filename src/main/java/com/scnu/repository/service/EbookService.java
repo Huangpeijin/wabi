5663f797fa341b7e61e3cb5fb0fbda5718e92889
Huangpeijin <kim475@163.com>
2023-02-04 13:15:52 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace最低级别

--- src/main/java/com/scnu/repository/service/EbookService.java
+++ src/main/java/com/scnu/repository/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.scnu.repository.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.scnu.repository.domain.Ebook;
 import com.scnu.repository.domain.EbookExample;
 import com.scnu.repository.mapper.EbookMapper;
 import com.scnu.repository.req.EbookReq;
 import com.scnu.repository.resp.EbookResp;
 import com.scnu.repository.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,15 +18,22 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
     public List<EbookResp> list(EbookReq req){
+        //在这个列表接口设置支持分页,两个参数，查第一页，每页查三条，现在这个查询就支持分页了。
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
+        PageHelper.startPage(1,2);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
