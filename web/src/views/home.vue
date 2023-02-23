f94931f74bb786fde7fc0e61f7efcd29451b11dd
Huangpeijin <kim475@163.com>
2023-02-23 09:53:54 +0800

    Feat(seven/seven):The home page displays the category menu

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -2,47 +2,28 @@
   <a-layout class="middle">
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-              v-model:selectedKeys="selectedKeys2"
-              v-model:openKeys="openKeys"
               mode="inline"
-              style="height: 100%"
+              :style="{ height: '100%', borderRight: 0 }"
+              @click="handleClick"
+              :openKeys="openKeys"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 11111
-                </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
+        <a-menu-item key="tip">
+          <span>以上菜单在分类管理配置</span>
+        </a-menu-item>
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{background:'#fff', padding: '24px', minHeight: '280px' }">
@@ -72,6 +53,8 @@
 
 import { defineComponent, onMounted,ref,reactive} from 'vue';
 import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 // const listData : any = [];
 // for (let i = 0; i < 23; i++) {
@@ -91,7 +74,34 @@ export default defineComponent({
   setup(){
       const ebooks =ref();
       const ebooks1 =reactive({books:[]});
+    const level1 =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          // // 加载完分类后，将侧边栏全部展开
+          // openKeys.value = [];
+          // for (let i = 0; i < categorys.length; i++) {
+          //   openKeys.value.push(categorys[i].id)
+          // }
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
       onMounted(()=>{
+        handleQueryCategory();
         axios.get("/ebook/list",{
           params:{
             page:1,
@@ -117,7 +127,9 @@ export default defineComponent({
           { type: 'StarOutlined', text: '156' },
           { type: 'LikeOutlined', text: '156' },
           { type: 'MessageOutlined', text: '2' },
-        ]
+        ],
+
+        level1,
     }
   }
 })
