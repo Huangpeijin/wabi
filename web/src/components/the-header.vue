75154b240f457101569edf328f49c6540ad28754
Huangpeijin <kim475@163.com>
2023-02-24 21:25:16 +0800

    Feat(9/9):前端显示登陆昵称，遗留问题，刷新会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,7 +22,10 @@
             <a-menu-item key="/about">
                 <router-link to="/about">关于我们</router-link>
             </a-menu-item>
-            <a class="login-menu" @click="showLoginModal">
+            <a class="login-menu" v-show="user.id">
+                <span>您好:{{user.name}}</span>
+            </a>
+            <a class="login-menu" @click="showLoginModal" v-show="!user.id">
                 <span>登录</span>
             </a>
         </a-menu>
@@ -56,7 +59,8 @@
         setup () {
             // // 登录后保存
             // const user = computed(() => store.state.user);
-
+            const user =ref();
+            user.value={};
             // 用来登录
             const loginUser = ref({
                 loginName: "test",
@@ -79,6 +83,7 @@
                     if (data.success) {
                         loginModalVisible.value = false;
                         message.success("登录成功！");
+                        user.value=data.content;
 
                         // store.commit("setUser", data.content);
                     } else {
@@ -107,7 +112,7 @@
                 showLoginModal,
                 loginUser,
                 login,
-                // user,
+                user,
                 // logout
             }
         }
