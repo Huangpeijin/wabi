51ea22b8296db70fd87a1b488273b38ffbf53fe8
Huangpeijin <kim475@163.com>
2023-02-24 14:08:47 +0800

    Fix(9/2):用户表设计并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -110,3 +110,15 @@ create table `content`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment ='文档内容';
+
+# 用户表 unique是唯一键，登录名不能重复
+drop table if exists `user`;
+create table `user`
+(
+    `id`       bigint      not null comment 'ID',
+    `login_name` varchar(50) not null Comment '登陆名',
+    `name`     varchar(50) Comment '昵称',
+    `password` char(32)    not null comment '密码',
+    primary key (id),
+    unique key login_name_unique (login_name)
+) engine = innodb default charset = utf8mb4 comment =' 用户';
