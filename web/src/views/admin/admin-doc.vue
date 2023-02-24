269d2a81176482b290790aae3c5226e3110aef23
Huangpeijin <kim475@163.com>
2023-02-24 11:00:16 +0800

    Feat(8/13):给文档管理增加了内容预览功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -79,11 +79,19 @@
                             <a-input v-model:value="doc.sort" placeholder="顺序"/>
                         </a-form-item>
                         <a-form-item>
+                            <a-button type="primary" @click="handlePreviewContent()">
+                                <EyeOutlined /> 内容预览
+                            </a-button>
+                        </a-form-item>
+                        <a-form-item>
                             <div id="content"></div>
                         </a-form-item>
                     </a-form>
                 </a-col>
             </a-row>
+            <a-drawer width="900" placement="right" :closable="false" :visible="drawerVisible" @close="onDrawerClose">
+                <div class="wangeditor" :innerHTML="previewHtml"></div>
+            </a-drawer>
         </a-layout-content>
     </a-layout>
 <!--    <a-modal
@@ -340,6 +348,17 @@
                     },
                 });
             };
+            // ----------------富文本预览--------------
+            const drawerVisible = ref(false);
+            const previewHtml = ref();
+            const handlePreviewContent = () => {
+                const html = editor.txt.html();
+                previewHtml.value = html;
+                drawerVisible.value = true;
+            };
+            const onDrawerClose = () => {
+                drawerVisible.value = false;
+            };
             onMounted(() => {
                 handleQuery();
                 editor.create();//创建富文本
@@ -361,7 +380,13 @@
                 modalLoading,
                 handleSave,
 
-                treeSelectData
+                treeSelectData,
+
+
+                drawerVisible,
+                previewHtml,
+                handlePreviewContent,
+                onDrawerClose,
             }
         }
     });
