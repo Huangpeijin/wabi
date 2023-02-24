df4eae04c9ae9242e6518e146ffacb3c1beacad4
Huangpeijin <kim475@163.com>
2023-02-24 19:23:51 +0800

    Feat(9/9):增加了redis接口测试，Key和calue是为空，可能是没有购买redis的原因

--- src/main/java/com/scnu/repository/controller/TestController.java
+++ src/main/java/com/scnu/repository/controller/TestController.java
@@ -1,21 +1,28 @@
 package com.scnu.repository.controller;
 
+import ch.qos.logback.classic.Logger;
 import com.scnu.repository.domain.Test;
 import com.scnu.repository.service.TestService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = (Logger) LoggerFactory.getLogger(TestController.class);
     @Value("${test.hello:TEST}")
     private  String testhello;
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
     @GetMapping("/hello")
     public String hello(){
         return  "hello world!"+testhello;
@@ -27,5 +34,19 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
