d9a5c0890dafc25018d1708ba0abd9fcf0d147a1
Huangpeijin <kim475@163.com>
2023-02-25 16:14:49 +0800

    Feat(9/11):增加退出登录功能,退出登录时,前端清除store登录信息,后端清除redis登录信息。

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -22,9 +22,20 @@
             <a-menu-item key="/about">
                 <router-link to="/about">关于我们</router-link>
             </a-menu-item>
+            <a-popconfirm
+                    title="确认退出登录?"
+                    ok-text="是"
+                    cancel-text="否"
+                    @confirm="logout()"
+            >
+                <a class="login-menu" v-show="user.id">
+                    <span>退出登录</span>
+                </a>
+            </a-popconfirm>
             <a class="login-menu" v-show="user.id">
                 <span>您好:{{user.name}}</span>
             </a>
+
             <a class="login-menu" @click="showLoginModal" v-show="!user.id">
                 <span>登录</span>
             </a>
@@ -98,19 +109,19 @@
                 });
             };
 
-            // 退出登录
-            // const logout = () => {
-            //     console.log("退出登录开始");
-            //     // axios.get('/user/logout/' + user.value.token).then((response) => {
-            //     //     const data = response.data;
-            //     //     if (data.success) {
-            //     //         message.success("退出登录成功！");
-            //     //         store.commit("setUser", {});
-            //     //     } else {
-            //     //         message.error(data.message);
-            //     //     }
-            //     // });
-            // };
+            //退出登录
+            const logout = () => {
+                console.log("退出登录开始");
+                axios.get('/user/logout/' + user.value.token).then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        message.success("退出登录成功！");
+                        store.commit("setUser", {});
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
 
             return {
                 loginModalVisible,
@@ -120,7 +131,7 @@
                 login,
                 user,
                 // user1,
-                // logout
+                logout
             }
         }
     });
