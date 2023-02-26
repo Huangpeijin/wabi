4d33c1f0fa5a3aa3ace3a58926666a777a2f2439
Huangpeijin <kim475@163.com>
2023-02-26 10:43:42 +0800

    Feat(10/6):首页显示电子书信息:文档数、阅读数、点赞数。 doc/vote/**接口不需要登录拦截

--- src/main/java/com/scnu/repository/config/SpringMvcConfig.java
+++ src/main/java/com/scnu/repository/config/SpringMvcConfig.java
@@ -27,7 +27,7 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/category/all",
                         "/ebook/list",
                         "/doc/all/**",
-//                        "/doc/vote/**",
+                        "/doc/vote/**",
                         "/doc/find-content/**",
                         "/ebook-snapshot/**"
                 );
