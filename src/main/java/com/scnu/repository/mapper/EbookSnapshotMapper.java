0ea78e693a8445b1006fbb903531393618320cb0
Huangpeijin <kim475@163.com>
2023-03-05 19:14:23 +0800

    Feat(11-3):增加电子书快照表，生成持久层代码

---
+++ src/main/java/com/scnu/repository/mapper/EbookSnapshotMapper.java
@@ -0,0 +1,30 @@
+package com.scnu.repository.mapper;
+
+import com.scnu.repository.domain.EbookSnapshot;
+import com.scnu.repository.domain.EbookSnapshotExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookSnapshotMapper {
+    long countByExample(EbookSnapshotExample example);
+
+    int deleteByExample(EbookSnapshotExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(EbookSnapshot record);
+
+    int insertSelective(EbookSnapshot record);
+
+    List<EbookSnapshot> selectByExample(EbookSnapshotExample example);
+
+    EbookSnapshot selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByExample(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByPrimaryKeySelective(EbookSnapshot record);
+
+    int updateByPrimaryKey(EbookSnapshot record);
+}
